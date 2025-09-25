0381525e50b42f0d2c9c3065bc8b10b1f8d6c952
Vitor Fagundes <vitorfagundes.7@gmail.com>
2025-09-25 19:50:54 -0300

    iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const {expect} = require('chai');
+
+// mocha para estrtuturar o teste e executa-lo
+describe('Login', () => {
+    describe('POST', () => {
+        it('Deve retornar 200 com token em string quando usar credenciais validas', async () => {
+            // usar supertest para fazer requisição a API de transferencias bancarias
+            const resposta = await request('http://localhost:3000')
+                .post('login')
+                .set('Content-type', 'Application/json')
+                .send({
+                    'username': 'vitor.anchieta',
+                    'senha': '123456'
+                })
+
+            //usar chai para fazer asserções    
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.token).to.be.a('string'); //validar que token é do tipo string
+        })
+    })
+})
